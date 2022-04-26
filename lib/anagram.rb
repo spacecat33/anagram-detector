a52dc3e8449cd47e9b749b64b1eb46a0938de491
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-26 17:58:29 +0100

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        new_array = []
+        array.each do |possible_anagram|
+            new_array << possible_anagram if possible_anagram.split("").sort == @word.split("").sort
+        end
+        new_array
+    end
+
+    #or
+    # def match(array)
+    #    array.select {|x| x.split("").sort == word.split("").sort}
+    # end 
+
+end
+
+#Note: not sure how to factor in #%w(some words) into the solution.
